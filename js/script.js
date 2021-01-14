a516d8263f72018ef0a7eee7a1048e29c8f017ed
Aditee Singh <singhaditee33@gmail.com>
2021-01-14 21:01:17 +0530

    Modified functionality in calendar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 const date = new Date();
 
+const renderCalendar = () =>{
+    date.setDate(1);
+console.log(date.getDay());
+
+const lastDay = new Date(date.getFullYear(),date.getMonth()+1,0).getDate();
+console.log(lastDay);
+
+const prevLastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate();
+
+const lastDayIndex = new Date(date.getFullYear(),date.getMonth()+1,0).getDay();
+
+const nextDays = 7-lastDayIndex-1;
+
+const firstDayIndex = date.getDay();
+
 const months = [
     "January",
     "February",
@@ -17,13 +32,41 @@ const months = [
 
 console.log(date.getMonth());
 document.querySelector(".date h1").innerHTML = months[date.getMonth()];
-document.querySelector(".date p").innerHTML = date.toDateString();
+document.querySelector(".date p").innerHTML = new Date().toDateString();
 
 const monthDays = document.querySelector(".days");
 
 let days = "";
 
-for( var i = 1; i <= 31; i++){
-    days+= `<div>${i}</div>`;
+for( let x = firstDayIndex; x > 0; x--){
+    days+= `<div class="prev-date">${prevLastDay - x + 1}</div>` 
+}
+
+for (let i = 1; i <= lastDay; i++){
+    if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
+      days += `<div class="today">${i}</div>`;
+    } else {
+      days += `<div>${i}</div>`;
+    }
+  }
+
+for(var j = 1; j <= nextDays; j++){
+    days+= `<div class="next-date">${j}</div>`;
     monthDays.innerHTML = days;
 }
+
+}
+// date.setMonth(5);
+
+document.querySelector(".prev").addEventListener("click", () => {
+    date.setMonth(date.getMonth() - 1);
+    renderCalendar();
+  });
+  
+  document.querySelector(".next").addEventListener("click", () => {
+    date.setMonth(date.getMonth() + 1);
+    renderCalendar();
+  });
+  
+  renderCalendar();
+  
